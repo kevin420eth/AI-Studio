db7ebee5f1cf7b2d44ec827229583f3384208754
Kevin Mars <kevin.eth.420@gmail.com>
2023-04-13 20:26:50 +0800

    sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import SideBar from './components/SideBar/SideBar';
 
