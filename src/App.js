ac9bc064bf0f25c9f8f23eb52cbbec935bce8b28
Kevin Mars <kevin.eth.420@gmail.com>
2023-04-13 15:57:35 +0800

    config react route in app.js and index.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home/>} />
-        
+        <Route path='/home' element={<Home/>} />
+        <Route path='/' element={<Header/>} />
       </Routes>
     </div>
   );
