e98f61d8b90a29e1ab685e0c5505c41396850e40
Kevin Mars <kevin.eth.420@gmail.com>
2023-04-13 23:12:41 +0800

    showcase page completed 25%

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
+import { useParams } from 'react-router'
 import './showcase.css'
 import SideBar from '../SideBar/SideBar'
 
 const Showcase = () => {
-    return (
-        <section className='relative'>
-            <SideBar />
-            <div className='bg-gradient-to-br from-slate-800 to-slate-900 absolute w-10/12 min-h-screen right-0 text-center pt-4'>
-                <h1 className='text-4xl'>Photography</h1>
+    let { artStyle } = useParams()
+
+    const styles = ["photography", "painting", "anime", "animal", "landscape", "architecture"]
+
+    if (styles.includes(artStyle)) {
+        artStyle=artStyle.charAt(0).toUpperCase()+artStyle.slice(1)
+        
+        return (
+            <section className='relative'>
+                <SideBar />
+                <div className='bg-gradient-to-br from-slate-800 to-slate-900 absolute w-10/12 min-h-screen right-0 text-center pt-4'>
+                    <h1 className='text-4xl'>{artStyle}</h1>
+                </div>
+            </section>
+        )
+    } else {
+        console.log('ddsds')
+        return (
+            <div className='text-black'>
+                <h1 className='mb-8'>Page not found</h1>
+                <a className='text-blue-600 underline' href="/photography">Go Back 🏠</a>
             </div>
-        </section>
-    )
+        )
+    }
+
+
 }
 
 export default Showcase
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -14,44 +14,43 @@ const SideBar = () => {
         <nav className='bg-slate-900 w-2/12 h-screen pl-3 pr-3 pt-8 flex flex-col gap-4 text-xl fixed'>
             <h1 className='mb-10 pb-4 border-b-2 select-none'>The New World</h1>
             <div className='menu flex flex-col gap-4'>
-                <a className='hover:bg-slate-600 focus:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="#1">
+                <a className='hover:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="/photography">
                     <MdPhotoCamera />
                     Photography
                 </a>
-                <a className='hover:bg-slate-600 focus:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="#1" >
+                <a className='hover:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="/painting" >
                     <AiFillFormatPainter />
                     Painting
                 </a>
-                <a className='hover:bg-slate-600 focus:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="#1" >
+                <a className='hover:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="/anime" >
                     <GiFox />
                     Anime
                 </a>
-                <a className='hover:bg-slate-600 focus:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="#1" >
+                <a className='hover:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="animal" >
                     <FaDog />
                     Animal
                 </a>
-                <a className='hover:bg-slate-600 focus:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="#1" >
+                <a className='hover:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="/landscape" >
                     <FaTree />
                     LandScape
                 </a>
 
-                <a className='hover:bg-slate-600 focus:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="#1" >
+                <a className='hover:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="/architecture" >
                     <BsFillHouseDoorFill />
                     Architecture
                 </a>
             </div>
 
-
-            <div className='social__section absolute bottom-4 translate-x-2/4 right-2/4 text-center'>
+            <div className='absolute bottom-4 translate-x-2/4 right-2/4 w-full text-center'>
                 <h1 className='mb-4'>{textbar}</h1>
-                <div className='flex justify-center items-center gap-6'>
-                    <a href="mailto:kevin23122@gmail.com" className='email__link hover:bg-slate-600 border-2 px-2 py-2 rounded-2xl text-4xl' onMouseEnter={() => { setTextBar('Email Us')}} onMouseLeave={()=>{setTextBar('Contact Us')}}>
+                <div className='flex justify-center items-center gap-4'>
+                    <a href="mailto:kevin23122@gmail.com" className='email__link hover:bg-slate-600 border-2 px-2 py-2 rounded-2xl text-3xl' onMouseEnter={() => { setTextBar('Email Us')}} onMouseLeave={()=>{setTextBar('Contact Us')}}>
                         <AiFillMail />
                     </a>
-                    <a href="https://shopee.tw/" className='newsletter__link hover:bg-slate-600 border-2 px-2 py-2 rounded-2xl text-4xl' onMouseEnter={() => { setTextBar('Subscribe Newsletter')}} onMouseLeave={()=>{setTextBar('Contact Us')}}>
+                    <a href="https://shopee.tw/" className='newsletter__link hover:bg-slate-600 border-2 px-2 py-2 rounded-2xl text-3xl' onMouseEnter={() => { setTextBar('Subscribe Newsletter')}} onMouseLeave={()=>{setTextBar('Contact Us')}}>
                         <ImNewspaper />
                     </a>
-                    <a href="https://shopee.tw/" target='_blank' rel="noreferrer" className='shopee__link hover:bg-slate-600 border-2 px-2 py-2 rounded-2xl text-4xl' onMouseEnter={() => { setTextBar('Go Shopee')}} onMouseLeave={()=>{setTextBar('Contact Us')}}>
+                    <a href="https://shopee.tw/" target='_blank' rel="noreferrer" className='shopee__link hover:bg-slate-600 border-2 px-2 py-2 rounded-2xl text-3xl' onMouseEnter={() => { setTextBar('Go Shopee')}} onMouseLeave={()=>{setTextBar('Contact Us')}}>
                         <MdOutlineAttachMoney />
                     </a>
                 </div>
