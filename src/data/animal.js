545567a9a0d965d1114744bc0668426e22cac8cf
Kevin Mars <kevin.eth.420@gmail.com>
2023-04-14 22:13:42 +0800

    mofified folder structure

--- src/data/animal.js
+++ src/data/animal.js
@@ -1,175 +1,213 @@
-import image1 from '../assets/art/photography/1.png'
-import image2 from '../assets/art/photography/2.png'
+// import image1 from '../assets/art/animal/1.png'
+// import image2 from '../assets/art/animal/2.png'
+// import image3 from '../assets/art/animal/3.png'
+// import image4 from '../assets/art/animal/4.png'
+// import image5 from '../assets/art/animal/5.png'
+// import image6 from '../assets/art/animal/6.png'
+// import image7 from '../assets/art/animal/7.png'
+// import image8 from '../assets/art/animal/8.png'
+// import image9 from '../assets/art/animal/9.png'
+// import image10 from '../assets/art/animal/10.png'
+// import image11 from '../assets/art/animal/11.png'
+// import image12 from '../assets/art/animal/12.png'
+// import image13 from '../assets/art/animal/13.png'
+// import image14 from '../assets/art/animal/14.png'
+// import image15 from '../assets/art/animal/15.png'
+// import image16 from '../assets/art/animal/16.png'
+// import image17 from '../assets/art/animal/17.png'
+// import image18 from '../assets/art/animal/18.png'
+// import image19 from '../assets/art/animal/19.png'
+// import image20 from '../assets/art/animal/20.png'
+// import image21 from '../assets/art/animal/21.png'
+// import image22 from '../assets/art/animal/22.png'
+// import image23 from '../assets/art/animal/23.png'
+// import image24 from '../assets/art/animal/24.png'
+// import image25 from '../assets/art/animal/25.png'
+// import image26 from '../assets/art/animal/26.png'
+// import image27 from '../assets/art/animal/27.png'
+// import image28 from '../assets/art/animal/28.png'
+// import image29 from '../assets/art/animal/29.png'
+// import image30 from '../assets/art/animal/30.png'
+// import image31 from '../assets/art/animal/31.png'
+// import image32 from '../assets/art/animal/32.png'
+// import image33 from '../assets/art/animal/33.png'
+// import image34 from '../assets/art/animal/34.png'
+// import image35 from '../assets/art/animal/35.png'
+// import image36 from '../assets/art/animal/36.png'
+// import image37 from '../assets/art/animal/37.png'
+// import image38 from '../assets/art/animal/38.png'
+// import image39 from '../assets/art/animal/39.png'
+// import image40 from '../assets/art/animal/40.png'
 
-const data = [
-    {   
-        'id':1,
-        'image':image1
-    },
-    {
-        'id':2,
-        'image':image2
-    },
-    {   
-        'id':3,
-        'image':image1
-    },
-    {
-        'id':4,
-        'image':image2
-    },
-    {   
-        'id':5,
-        'image':image1
-    },
-    {
-        'id':6,
-        'image':image2
-    },
-    {   
-        'id':7,
-        'image':image1
-    },
-    {
-        'id':8,
-        'image':image2
-    },
-    {   
-        'id':9,
-        'image':image1
-    },
-    {
-        'id':10,
-        'image':image2
-    },
-    {   
-        'id':11,
-        'image':image1
-    },
-    {
-        'id':12,
-        'image':image2
-    },
-    {   
-        'id':13,
-        'image':image1
-    },
-    {
-        'id':14,
-        'image':image2
-    },
-    {   
-        'id':15,
-        'image':image1
-    },
-    {
-        'id':16,
-        'image':image2
-    },
-    {   
-        'id':17,
-        'image':image1
-    },
-    {
-        'id':18,
-        'image':image2
-    },
-    {   
-        'id':19,
-        'image':image1
-    },
-    {
-        'id':20,
-        'image':image2
-    },
-    {   
-        'id':21,
-        'image':image1
-    },
-    {
-        'id':22,
-        'image':image2
-    },
-    {   
-        'id':23,
-        'image':image1
-    },
-    {
-        'id':24,
-        'image':image2
-    },
-    {   
-        'id':25,
-        'image':image1
-    },
-    {
-        'id':26,
-        'image':image2
-    },
-    {   
-        'id':27,
-        'image':image1
-    },
-    {
-        'id':28,
-        'image':image2
-    },
-    {   
-        'id':29,
-        'image':image1
-    },
-    {
-        'id':30,
-        'image':image2
-    },
-    {   
-        'id':31,
-        'image':image1
-    },
-    {
-        'id':32,
-        'image':image2
-    },
-    {   
-        'id':33,
-        'image':image1
-    },
-    {
-        'id':34,
-        'image':image2
-    },
-    {   
-        'id':35,
-        'image':image1
-    },
-    {
-        'id':36,
-        'image':image2
-    },
-    {   
-        'id':37,
-        'image':image1
-    },
-    {
-        'id':38,
-        'image':image2
-    },
-    {   
-        'id':39,
-        'image':image1
-    },
-    {
-        'id':40,
-        'image':image2
-    },
-    {
-        'id':41,
-        'image':image1
-    },
-    {
-        'id':42,
-        'image':image2
-    }
-]
+// const data = [
+//     {   
+//         'id':1,
+//         'image':image1
+//     },
+//     {
+//         'id':2,
+//         'image':image2
+//     },
+//     {   
+//         'id':3,
+//         'image':image1
+//     },
+//     {
+//         'id':4,
+//         'image':image2
+//     },
+//     {   
+//         'id':5,
+//         'image':image1
+//     },
+//     {
+//         'id':6,
+//         'image':image2
+//     },
+//     {   
+//         'id':7,
+//         'image':image1
+//     },
+//     {
+//         'id':8,
+//         'image':image2
+//     },
+//     {   
+//         'id':9,
+//         'image':image1
+//     },
+//     {
+//         'id':10,
+//         'image':image2
+//     },
+//     {   
+//         'id':11,
+//         'image':image1
+//     },
+//     {
+//         'id':12,
+//         'image':image2
+//     },
+//     {   
+//         'id':13,
+//         'image':image1
+//     },
+//     {
+//         'id':14,
+//         'image':image2
+//     },
+//     {   
+//         'id':15,
+//         'image':image1
+//     },
+//     {
+//         'id':16,
+//         'image':image2
+//     },
+//     {   
+//         'id':17,
+//         'image':image1
+//     },
+//     {
+//         'id':18,
+//         'image':image2
+//     },
+//     {   
+//         'id':19,
+//         'image':image1
+//     },
+//     {
+//         'id':20,
+//         'image':image2
+//     },
+//     {   
+//         'id':21,
+//         'image':image1
+//     },
+//     {
+//         'id':22,
+//         'image':image2
+//     },
+//     {   
+//         'id':23,
+//         'image':image1
+//     },
+//     {
+//         'id':24,
+//         'image':image2
+//     },
+//     {   
+//         'id':25,
+//         'image':image1
+//     },
+//     {
+//         'id':26,
+//         'image':image2
+//     },
+//     {   
+//         'id':27,
+//         'image':image1
+//     },
+//     {
+//         'id':28,
+//         'image':image2
+//     },
+//     {   
+//         'id':29,
+//         'image':image1
+//     },
+//     {
+//         'id':30,
+//         'image':image2
+//     },
+//     {   
+//         'id':31,
+//         'image':image1
+//     },
+//     {
+//         'id':32,
+//         'image':image2
+//     },
+//     {   
+//         'id':33,
+//         'image':image1
+//     },
+//     {
+//         'id':34,
+//         'image':image2
+//     },
+//     {   
+//         'id':35,
+//         'image':image1
+//     },
+//     {
+//         'id':36,
+//         'image':image2
+//     },
+//     {   
+//         'id':37,
+//         'image':image1
+//     },
+//     {
+//         'id':38,
+//         'image':image2
+//     },
+//     {   
+//         'id':39,
+//         'image':image1
+//     },
+//     {
+//         'id':40,
+//         'image':image2
+//     },
+//     {
+//         'id':41,
+//         'image':image1
+//     },
+//     {
+//         'id':42,
+//         'image':image2
+//     }
+// ]
 
-export default data
+// export default data
--- src/data/landscape.js
+++ src/data/landscape.js
@@ -1,175 +1,213 @@
-import image1 from '../assets/art/photography/1.png'
-import image2 from '../assets/art/photography/2.png'
+// import image1 from '../assets/art/landscape/1.png'
+// import image2 from '../assets/art/landscape/2.png'
+// import image3 from '../assets/art/landscape/3.png'
+// import image4 from '../assets/art/landscape/4.png'
+// import image5 from '../assets/art/landscape/5.png'
+// import image6 from '../assets/art/landscape/6.png'
+// import image7 from '../assets/art/landscape/7.png'
+// import image8 from '../assets/art/landscape/8.png'
+// import image9 from '../assets/art/landscape/9.png'
+// import image10 from '../assets/art/landscape/10.png'
+// import image11 from '../assets/art/landscape/11.png'
+// import image12 from '../assets/art/landscape/12.png'
+// import image13 from '../assets/art/landscape/13.png'
+// import image14 from '../assets/art/landscape/14.png'
+// import image15 from '../assets/art/landscape/15.png'
+// import image16 from '../assets/art/landscape/16.png'
+// import image17 from '../assets/art/landscape/17.png'
+// import image18 from '../assets/art/landscape/18.png'
+// import image19 from '../assets/art/landscape/19.png'
+// import image20 from '../assets/art/landscape/20.png'
+// import image21 from '../assets/art/landscape/21.png'
+// import image22 from '../assets/art/landscape/22.png'
+// import image23 from '../assets/art/landscape/23.png'
+// import image24 from '../assets/art/landscape/24.png'
+// import image25 from '../assets/art/landscape/25.png'
+// import image26 from '../assets/art/landscape/26.png'
+// import image27 from '../assets/art/landscape/27.png'
+// import image28 from '../assets/art/landscape/28.png'
+// import image29 from '../assets/art/landscape/29.png'
+// import image30 from '../assets/art/landscape/30.png'
+// import image31 from '../assets/art/landscape/31.png'
+// import image32 from '../assets/art/landscape/32.png'
+// import image33 from '../assets/art/landscape/33.png'
+// import image34 from '../assets/art/landscape/34.png'
+// import image35 from '../assets/art/landscape/35.png'
+// import image36 from '../assets/art/landscape/36.png'
+// import image37 from '../assets/art/landscape/37.png'
+// import image38 from '../assets/art/landscape/38.png'
+// import image39 from '../assets/art/landscape/39.png'
+// import image40 from '../assets/art/landscape/40.png'
 
-const data = [
-    {   
-        'id':1,
-        'image':image1
-    },
-    {
-        'id':2,
-        'image':image2
-    },
-    {   
-        'id':3,
-        'image':image1
-    },
-    {
-        'id':4,
-        'image':image2
-    },
-    {   
-        'id':5,
-        'image':image1
-    },
-    {
-        'id':6,
-        'image':image2
-    },
-    {   
-        'id':7,
-        'image':image1
-    },
-    {
-        'id':8,
-        'image':image2
-    },
-    {   
-        'id':9,
-        'image':image1
-    },
-    {
-        'id':10,
-        'image':image2
-    },
-    {   
-        'id':11,
-        'image':image1
-    },
-    {
-        'id':12,
-        'image':image2
-    },
-    {   
-        'id':13,
-        'image':image1
-    },
-    {
-        'id':14,
-        'image':image2
-    },
-    {   
-        'id':15,
-        'image':image1
-    },
-    {
-        'id':16,
-        'image':image2
-    },
-    {   
-        'id':17,
-        'image':image1
-    },
-    {
-        'id':18,
-        'image':image2
-    },
-    {   
-        'id':19,
-        'image':image1
-    },
-    {
-        'id':20,
-        'image':image2
-    },
-    {   
-        'id':21,
-        'image':image1
-    },
-    {
-        'id':22,
-        'image':image2
-    },
-    {   
-        'id':23,
-        'image':image1
-    },
-    {
-        'id':24,
-        'image':image2
-    },
-    {   
-        'id':25,
-        'image':image1
-    },
-    {
-        'id':26,
-        'image':image2
-    },
-    {   
-        'id':27,
-        'image':image1
-    },
-    {
-        'id':28,
-        'image':image2
-    },
-    {   
-        'id':29,
-        'image':image1
-    },
-    {
-        'id':30,
-        'image':image2
-    },
-    {   
-        'id':31,
-        'image':image1
-    },
-    {
-        'id':32,
-        'image':image2
-    },
-    {   
-        'id':33,
-        'image':image1
-    },
-    {
-        'id':34,
-        'image':image2
-    },
-    {   
-        'id':35,
-        'image':image1
-    },
-    {
-        'id':36,
-        'image':image2
-    },
-    {   
-        'id':37,
-        'image':image1
-    },
-    {
-        'id':38,
-        'image':image2
-    },
-    {   
-        'id':39,
-        'image':image1
-    },
-    {
-        'id':40,
-        'image':image2
-    },
-    {
-        'id':41,
-        'image':image1
-    },
-    {
-        'id':42,
-        'image':image2
-    }
-]
+// const data = [
+//     {   
+//         'id':1,
+//         'image':image1
+//     },
+//     {
+//         'id':2,
+//         'image':image2
+//     },
+//     {   
+//         'id':3,
+//         'image':image1
+//     },
+//     {
+//         'id':4,
+//         'image':image2
+//     },
+//     {   
+//         'id':5,
+//         'image':image1
+//     },
+//     {
+//         'id':6,
+//         'image':image2
+//     },
+//     {   
+//         'id':7,
+//         'image':image1
+//     },
+//     {
+//         'id':8,
+//         'image':image2
+//     },
+//     {   
+//         'id':9,
+//         'image':image1
+//     },
+//     {
+//         'id':10,
+//         'image':image2
+//     },
+//     {   
+//         'id':11,
+//         'image':image1
+//     },
+//     {
+//         'id':12,
+//         'image':image2
+//     },
+//     {   
+//         'id':13,
+//         'image':image1
+//     },
+//     {
+//         'id':14,
+//         'image':image2
+//     },
+//     {   
+//         'id':15,
+//         'image':image1
+//     },
+//     {
+//         'id':16,
+//         'image':image2
+//     },
+//     {   
+//         'id':17,
+//         'image':image1
+//     },
+//     {
+//         'id':18,
+//         'image':image2
+//     },
+//     {   
+//         'id':19,
+//         'image':image1
+//     },
+//     {
+//         'id':20,
+//         'image':image2
+//     },
+//     {   
+//         'id':21,
+//         'image':image1
+//     },
+//     {
+//         'id':22,
+//         'image':image2
+//     },
+//     {   
+//         'id':23,
+//         'image':image1
+//     },
+//     {
+//         'id':24,
+//         'image':image2
+//     },
+//     {   
+//         'id':25,
+//         'image':image1
+//     },
+//     {
+//         'id':26,
+//         'image':image2
+//     },
+//     {   
+//         'id':27,
+//         'image':image1
+//     },
+//     {
+//         'id':28,
+//         'image':image2
+//     },
+//     {   
+//         'id':29,
+//         'image':image1
+//     },
+//     {
+//         'id':30,
+//         'image':image2
+//     },
+//     {   
+//         'id':31,
+//         'image':image1
+//     },
+//     {
+//         'id':32,
+//         'image':image2
+//     },
+//     {   
+//         'id':33,
+//         'image':image1
+//     },
+//     {
+//         'id':34,
+//         'image':image2
+//     },
+//     {   
+//         'id':35,
+//         'image':image1
+//     },
+//     {
+//         'id':36,
+//         'image':image2
+//     },
+//     {   
+//         'id':37,
+//         'image':image1
+//     },
+//     {
+//         'id':38,
+//         'image':image2
+//     },
+//     {   
+//         'id':39,
+//         'image':image1
+//     },
+//     {
+//         'id':40,
+//         'image':image2
+//     },
+//     {
+//         'id':41,
+//         'image':image1
+//     },
+//     {
+//         'id':42,
+//         'image':image2
+//     }
+// ]
 
-export default data
+// export default data
