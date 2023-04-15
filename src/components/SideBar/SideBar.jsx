1ea533e60e367cce7fe724c6dadc56d98b348cb7
Kevin Mars <kevin.eth.420@gmail.com>
2023-04-16 06:15:01 +0800

    modify logo and favicon and the website title

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -5,14 +5,17 @@ import { FaDog, FaTree } from 'react-icons/fa'
 import { GiNinjaHead } from 'react-icons/gi'
 import { ImNewspaper } from 'react-icons/im'
 import { MdPhotoCamera, MdOutlineAttachMoney } from 'react-icons/md'
-
+import logo from '../../assets/materials/logo.png'
 
 const SideBar = () => {
     const [textbar, setTextBar] = useState('聯絡我們')
 
     return (
         <nav className='bg-slate-900 w-2/12 h-screen pl-3 pr-3 pt-8 flex flex-col gap-4 text-xl fixed desktop:hidden'>
-            <h1 className='mb-10 pb-4 border-b-2 select-none leading-8'>新世界 <br/>A.I. 工作室</h1>
+            <div>
+                <div className='mb-10 pb-4 border-b-2 select-none leading-8 flex justify-start items-center gap-4'>新世界 <br />A.I. 工作室<img className='w-14' src={logo} alt="logo_img" /></div>
+            </div>
+
             <div className='menu flex flex-col gap-4'>
                 <a className='hover:bg-slate-600 w-11/12 rounded-lg px-2 py-2 flex items-center gap-2' href="/photography">
                     <MdPhotoCamera />
